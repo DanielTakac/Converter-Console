f6995333a90f3389d25c4c76e76f78eafa2510ff
DanielTakac <danieltakac@protonmail.com>
2023-01-18 14:52:01 +0100

    Added color text class

---
+++ Converter/ColorText.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter {
+
+    public class ColorText {
+
+        public static void WriteLineColor(string input, ConsoleColor color) {
+
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(input);
+
+            Console.ForegroundColor = currentColor;
+
+        }
+
+        public static void WriteColor(string input, ConsoleColor color) {
+
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            Console.Write(input);
+
+            Console.ForegroundColor = currentColor;
+
+        }
+
+        public static string ReadLineColor(ConsoleColor color) {
+
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            string output = Console.ReadLine();
+
+            Console.ForegroundColor = currentColor;
+
+            return output;
+
+        }
+
+        public static string ReadColor(ConsoleColor color) {
+
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            string output = Console.Read().ToString();
+
+            Console.ForegroundColor = currentColor;
+
+            return output;
+
+        }
+
+        public static char ReadKeyColor(ConsoleColor color) {
+
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            char output = Console.ReadKey().KeyChar;
+
+            Console.ForegroundColor = currentColor;
+
+            return output;
+
+        }
+
+    }
+
+}
